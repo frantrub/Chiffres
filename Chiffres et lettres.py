e283500db187c9d1c7a8dc0e57a244c9a9d00997
François Trublereau <francois.trublereau@hec.edu>
2019-12-15 19:43:25 +0100

    fonction trouve la solution, mais n'indique pas le chemin et donne plusieurs fois les mêmes solutions, avec les mêmes calculs dans des ordresdifférents

--- Chiffres et lettres.py
+++ Chiffres et lettres.py
@@ -17,22 +17,26 @@ nombre_cherché=random.randint(100,1000)
 def calculs_possibles(cartes_restantes):
     N=len(cartes_restantes)
     for i in range(N-1):
-        for j in range(i,N):
-            a=cartes_restantes[i]+cartes_restantes[j]
-            b=cartes_restantes[i]*cartes_restantes[j]
-            c=np.abs(cartes_restantes[i]-cartes_restantes[j])
-            d=(max(cartes_restantes[i],cartes_restantes[j])/
-               min(cartes_restantes[i],cartes_restantes[j]))
-            if a==nombre_cherché or b==nombre_cherché or c==nombre_cherché or d==nombre_cherché:
+        for j in range(i+1,N):
+            x=cartes_restantes[i]
+            y=cartes_restantes[j]
+            a=x+y
+            b=x*y
+            c=np.abs(x-y)
+            d=max(x,y)//min(x,x)
+            if a==nombre_cherché or b==nombre_cherché or c==nombre_cherché :
                 print('solution trouvée')
             else:
-                nouvelles_cartes=[k 
-                              for k in cartes_restantes[:i]+cartes_restantes[j:]]
+                nouvelles_cartes=cartes_restantes[:i]+cartes_restantes[i+1:j]+cartes_restantes[j+1:]
+                assert len(nouvelles_cartes) == N-2
                 calculs_possibles(nouvelles_cartes+[a])
                 calculs_possibles(nouvelles_cartes+[b])
                 calculs_possibles(nouvelles_cartes+[c])
-                if d==int(d):
-                    calculs_possibles(nouvelles_cartes+[d])
+                if divmod(max(x,y),min(x,y))[1]==0:
+                    if d==nombre_cherché:
+                        print('solution trouvée')
+                    else : 
+                        calculs_possibles(nouvelles_cartes+[d])
             
             
                 
