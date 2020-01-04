9e524c728ba444dfe3a91b34d6f95ac0fb32018d
François Trublereau <francois.trublereau@hec.edu>
2020-01-04 18:48:55 +0100

    Fonction débuguée : 'utilise pas deux fois la  même carte

--- Chiffres et lettres.py
+++ Chiffres et lettres.py
@@ -10,42 +10,35 @@ import numpy as np
 import numpy.random as random 
 
 deck_de_cartes = np.array([1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,
-                           10,10,25,25,50,50,75,75,100,100])
-cartes_tirées = random.choice(deck_de_cartes, 4).tolist()
+                           10,10,25,50,75,100])
+cartes_tirées = random.choice(deck_de_cartes, 6).tolist()
 nombre_cherché = random.randint(100, 1000)
 
-def calculs_possibles(cartes_restantes, nombre_cherché):
-    if nombre_cherché in cartes_restantes:
+def calculs_possibles(cartes_disponibles, nombre_cherché):
+    
+    if nombre_cherché in cartes_disponibles:
         return "Le compte est bon !"
-    for i, x in enumerate(cartes_restantes):
-        for j, y in enumerate(cartes_restantes[i+1:], i+1):
-            nouvelles_cartes = (cartes_restantes[:i] +
-                  cartes_restantes[i+1:j] + cartes_restantes[j+1:])
-            
+    
+    for i in range(len(cartes_disponibles)):
+        for j in range(i+1, len(cartes_disponibles)):
+            x, y = cartes_disponibles[i], cartes_disponibles[j]
             x, y = max(x,y), min(x,y)
             
-            #Cas de l'addition :
-            essai = calculs_possibles(nouvelles_cartes + [x + y],
-                                 nombre_cherché)
-            if essai != None:
-                return str(x) + " + " + str(y) + " = " + str(x + y) + "\n" + essai
-            #Cas de la multiplication :
-            essai = calculs_possibles(nouvelles_cartes + [x * y],
-                                 nombre_cherché)
-            if essai != None:
-                return str(x) + " × " + str(y) + " = " + str(x * y) + "\n" + essai
-            #Cas de la soustraction :
-            essai = calculs_possibles(nouvelles_cartes + [x - y],
-                                 nombre_cherché)
-            if essai != None:
-                return str(x) + " - " + str(y) + " = " + str(x - y) + "\n"  + essai
-            #Cas de la division :
+            nouvelles_cartes = (cartes_disponibles[:i] +
+                  cartes_disponibles[i+1:j] + cartes_disponibles[j+1:])
+            
+            liste_des_opérateurs = [
+                [x + y, "+"], [x * y, "×"], [x - y, "-"]
+            ]
             if y != 0 and x % y == 0:
-                essai = calculs_possibles(
-                            nouvelles_cartes + [x // y],
-                            nombre_cherché)
+                liste_des_opérateurs.append([x // y, "÷"])
+                
+            for valeur, opérateur in liste_des_opérateurs: 
+                essai = calculs_possibles(nouvelles_cartes + [valeur], 
+                                          nombre_cherché)
                 if essai != None: 
-                    return str(x) + " ÷ " + str(y) + " = " + str(x // y) + "\n" + essai
+                    return (str(x) + " " + opérateur + " " + str(y)
+                            + " = " + str(valeur) + "\n" + essai)
     return None
           
 print("Nombre à trouver : " + str(nombre_cherché))
